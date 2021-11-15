370f244d1811767a7eac276d58d27557151c7a8c
aakashsadnani <A00235504@mycambrian.ca>
2021-11-15 18:47:38 -0500

    New Secure App Project Created
    A simple project of secure web app.

---
+++ WebAssignmentSecureWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebAssignmentSecureWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
